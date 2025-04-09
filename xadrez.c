541cca3d394bfedc807f329e6e9f6f766a9ed405
Elito00 <elito.5.0liveira@gmail.com>
2025-04-09 01:05:22 +0000

    xadrez v0.2

--- xadrez.c
+++ xadrez.c
@@ -11,6 +11,14 @@ int main() {
     printf("Movimento da Torre:\n");
     for (int i = 0; i < Torre; i++) {
         printf("Direita\n");
+    }
+
+    // Movimentação do Bispo usando while
+    // Bispo se move 5 casas na diagonal (Cima Direita)
+    printf("\nMovimento do Bispo:\n");
+    int i = 0;
+    while (i < Bispo) {
+        printf("Cima Direita\n");
 
 
     return 0;
